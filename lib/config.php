5bb97b0d986bf8bb46607d6371e8940738233bdf
Jorge Conde <jorgeconde@Jorges-iMac.local>
2015-10-12 11:20:06 -0500

    remove woocommerce from config.php

--- lib/config.php
+++ lib/config.php
@@ -39,7 +39,6 @@ function display_sidebar() {
     is_404(),
     is_front_page(),
     is_page_template('template-custom.php'),
-    is_woocommerce(),
   ]);
 
   return apply_filters('sage/display_sidebar', $display);
